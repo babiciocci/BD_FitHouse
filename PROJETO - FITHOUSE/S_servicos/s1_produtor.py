219722d4a59fe697e93797b3b8086dea2751f1c2
Bruno Arthur Basso Silva <148630072+BrunoBassoo@users.noreply.github.com>
2025-05-23 04:27:07 +0000

    fodase mn

--- PROJETO - FITHOUSE/S_servicos/s1_produtor.py
+++ PROJETO - FITHOUSE/S_servicos/s1_produtor.py
@@ -3,8 +3,9 @@ import time
 import json
 import random
 
+
 producer = KafkaProducer(
-    bootstrap_servers='kafka:9093',
+    bootstrap_servers='kafka:9092',
     api_version=(3,8,0),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
@@ -12,12 +13,11 @@ producer = KafkaProducer(
 if __name__ == '__main__':
     topic = 's1-s2'
 
-    for i in range(0,11):
-        mensagem = {
-            "dieta": "frango",
-            "treino": "biceps",
-            "usuario": {"id": 123}
-        }
-        print(mensagem)
-        producer.send(topic, value=mensagem)
-        producer.flush()
+while(True):
+    mensagem = {
+        "dieta": "frango",
+        "treino": "biceps",
+        "usuario": {"id": 123}
+    }
+    print(mensagem)
+    producer.send(topic, value=mensagem)
